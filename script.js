424098db928dac8504954a22144534a021ffeeb6
yurimanickam <79948608+yurimanickam@users.noreply.github.com>
2023-01-05 16:28:41 +1300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.clear();
 let scene = new THREE.Scene();
 //Load background texture
 const loader = new THREE.TextureLoader();
-loader.load('https://images.pexels.com/photos/1205301/pexels-photo-1205301.jpeg' , function(texture)
+loader.load('https://i.pinimg.com/originals/e7/dd/39/e7dd3948839cdfab04f64909dc4484d2.jpg' , function(texture)
             {
              scene.background = texture;  
             });
